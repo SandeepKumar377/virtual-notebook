6a702445628c47a0b3a1931a33ccb60cb4834fd8
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-09-16 16:19:22 +0530

    setup router and add basic components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import Home from './components/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Footerbar from './components/Footerbar';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <h1>This virtual Notbook</h1>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route>
+        </Switch>
+      </Router>
+      <Footerbar />
     </div>
   );
 }
