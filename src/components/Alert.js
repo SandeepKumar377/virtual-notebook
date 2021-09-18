bf2b7888f23167d18b4b180560e191869f646dc9
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-09-18 11:57:52 +0530

    add fontawsome library

---
+++ src/components/Alert.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Alert = () => {
+    return (
+        <div class="alert alert-danger" role="alert">
+            Thsi is alert component....!
+        </div>
+    )
+}
+export default Alert
